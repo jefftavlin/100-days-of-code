b76683c5dba50b21fd97df33d3a3fe274f5c8bcc
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-01 20:14:52 -0500

    Create Exercise-2-2.py

---
+++ Day #2/Exercise-2-2.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = int(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi = int(weight // (height ** 2))
+print(bmi)
