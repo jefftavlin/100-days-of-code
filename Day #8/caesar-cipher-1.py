9cabb039d79190cd4fcb708be939d5035469f894
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-05 09:20:08 -0500

    Update caesar-cipher-1.py

--- Day #8/caesar-cipher-1.py
+++ Day #8/caesar-cipher-1.py
@@ -5,7 +5,12 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def encrypt(text, shift):
-  shifted = [chr(ord(letter) + shift) for letter in text if ord(letter) < 128]
+  shifted = []
+  for letter in text:
+    if letter.isalpha() == True:
+      shifted.append(chr(ord(letter) + shift))
+    else:
+      shifted.append(letter)
   print(''.join(shifted))
 
 encrypt(text,shift)
