f4429fce037100d5586836238846860bd6e7bc51
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-05 09:14:05 -0500

    Create caesar-cipher-1.py

---
+++ Day #8/caesar-cipher-1.py
@@ -0,0 +1,11 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+  shifted = [chr(ord(letter) + shift) for letter in text if ord(letter) < 128]
+  print(''.join(shifted))
+
+encrypt(text,shift)
