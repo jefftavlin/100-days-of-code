23a0131d343949c7836da5057261c7074e09352b
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-05 11:24:11 -0500

    Create caesar-cipher-3.py

---
+++ Day #8/caesar-cipher-3.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(text, shift, direction):
+  shifted = []
+  for letter in text:
+    if letter.isalpha() == True:
+      if direction == 'encode':
+        shifted.append(chr(ord(letter) + shift))
+      elif direction == 'decode':
+        shifted.append(chr(ord(letter) - shift))
+    else:
+      shifted.append(letter)
+  print(''.join(shifted))
+
+caesar(text, shift, direction)
