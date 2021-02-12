57ed61dd604f4c9d0b7c74ce545b7b6c4d306cd9
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-12 11:24:15 -0500

    Create square.py

---
+++ Day #18/square.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("arrow")
+timmy.color('blue')
+
+for i in range(4):
+    timmy.forward(100)
+    timmy.right(90)
+    
+screen = Screen()
+screen.exitonclick()
