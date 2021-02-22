fd8d60d043c8ac4c6682c30b472439e957c0e111
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-22 17:44:21 -0500

    Create scoreboard.py

---
+++ Day #23/scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+class Scoreboard(Turtle):
+    def __init__(self, xcor):
+        super().__init__()
+        self.score = 0
+        self.goto(xcor, 225)
+        self.display_score()
+        self.color('white')
+        self.hideturtle()
+    def display_score(self):
+        self.clear()
+        self.write(f"{self.score}", align="center", font = ('Arial', 48, 'normal'))
+    def update_score(self):
+        self.score +=1
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER!", align="center", font = ('Arial', 48, 'normal'))
