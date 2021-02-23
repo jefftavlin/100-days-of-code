5ce8e122914bf7e7872918e5b347ea85349ea0ac
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-22 23:49:58 -0500

    Update setup.py

--- Day #23/setup.py
+++ Day #23/setup.py
@@ -1,19 +1,32 @@
-from turtle import Turtle
-import random
+from turtle import Turtle, Screen
 
 class Setup(Turtle):
     def __init__(self):
         super().__init__()
         self.screen = Screen()
-        self.screen.setup(width=600, height=600)
+        self.screen.setup(width=800, height=600)
         self.screen.bgcolor('black')
+        self.screen.tracer(0)
         self.screen.title('Pong')
+        self.line()
 
     def line(self):
         for i in range(19):
+            self.screen.tracer(0)
             line = Turtle('square')
-            line.speed(20)
+            line.speed(0)
             line.penup()
+            line.color('black')
             line.goto(0, 280 - (i * 30))
             line.shapesize(0.5, 0.3, 0.5)
             line.color('white')
+
+    def update(self):
+        self.screen.update()
+
+    def exit(self):
+        self.screen.exitonclick()
+
+    def onkey(self,func, key):
+        self.screen.listen()
+        self.screen.onkey(func, key)
