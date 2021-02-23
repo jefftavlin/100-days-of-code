d7e45361178e98c28dd5962f66d92be86a02ab23
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-22 23:48:41 -0500

    Update main.py

--- Day #23/main.py
+++ Day #23/main.py
@@ -3,15 +3,22 @@ import random
 import time
 from scoreboard import Scoreboard
 from setup import Setup
+from paddle import Paddle
 
 screen = Setup()
-
-screen.tracer(0)
-screen.listen()
-
 player_1_score = Scoreboard(-100)
 player_2_score = Scoreboard(100)
+p1_paddle = Paddle(350)
+p2_paddle = Paddle(-350)
 
+screen.onkey(p1_paddle.up, 'Up')
+screen.onkey(p1_paddle.down, 'Down')
 game = True
 
-screen.exitonclick()
+while game:
+    screen.update()
+    player_1_score.display_score()
+    player_2_score.display_score()
+    time.sleep(0.1)
+
+screen.exit()
