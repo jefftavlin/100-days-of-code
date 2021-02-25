f58b22a20cf51005462f3bb9a0cef68c707eb895
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-25 18:48:53 -0500

    Create map_game.py

---
+++ day-25/map_game.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import pandas as pd
+
+class Game:
+    def __init__(self, df, state_answer):
+        self.df = df
+        self.state_answer = state_answer
+        self.x, self.y = self.get_x_y()
+        self.place_on_map()
+
+    def get_x_y(self):
+        self.df = self.df[self.df.state == self.state_answer]
+        x = self.df['x'].values[0]
+        y = self.df['y'].values[0]
+        return x, y
+
+    def place_on_map(self):
+        state = Turtle()
+        state.goto(self.x, self.y)
