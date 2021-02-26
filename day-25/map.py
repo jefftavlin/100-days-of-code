bc5f56ae0b36a1952c7f4054bdd708b217e5969b
jefftavlin <61480915+jefftavlin@users.noreply.github.com>
2021-02-25 19:08:42 -0500

    Update map.py

--- day-25/map.py
+++ day-25/map.py
@@ -7,9 +7,14 @@ class Map(Turtle):
         self.screen.title('U.S States Game')
         self.image = 'blank_states_img.gif'
         self.screen.addshape(self.image)
+        self.screen.setup(width = 735, height = 491)
         self.shape(self.image)
+        self.score = 0
+        self.states_chosen = []
     def user_input(self):
-        self.answer = self.screen.textinput(title="Guess the State", prompt="Whats another state's name?").lower()
+        self.answer = self.screen.textinput(title=f"{self.score} / 50 States Correct", prompt="Whats another state's name?").lower()
+        if self.answer != 'quit':
+            self.states_chosen.append(self.answer)
         return self.answer
-    def place_state(self):
-        pass
+    def update_score(self):
+        self.score +=1
